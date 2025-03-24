ec3e1808605b37ea28331ca7f3f71b158e717133
gc <30398469+gc@users.noreply.github.com>
2025-03-24 11:22:13 +1100

    fix

--- run_all.sh
+++ run_all.sh
@@ -2,5 +2,5 @@
 set -e
 
 curl -sSL https://raw.githubusercontent.com/gc/scripts/master/checks.sh | bash
-curl -sSL https://raw.githubusercontent.com/gc/scripts/master/check_disk.sh | sudo bash
+#curl -sSL https://raw.githubusercontent.com/gc/scripts/master/check_disk.sh | sudo bash
 curl -sSL https://raw.githubusercontent.com/gc/scripts/master/check_network.sh | bash
